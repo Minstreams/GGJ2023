b12af6fb5ae9eef836a10bb8febc78c9ea5fe114
BlackShikra <49985982+BlackShikra@users.noreply.github.com>
2023-02-04 07:56:01 +0800

    sell

---
+++ Assets/Scripts/Options/OptionItemSell.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace IceEngine
+{
+    [CreateAssetMenu(fileName = "OptionItem", menuName = "物品选项/售卖建筑")]
+    public class OptionItemSell : OptionItem
+    {
+        public override void OnAct(Selectable obj)
+        {
+            if (obj is Building b)
+            {
+                int price = b.sellPrice;
+                Destroy(b.gameObject);
+                Ice.Gameplay.Money += price;
+            } 
+        }
+    }
+}
