decafa8856f9f6e14ecbd70092f66e278f35c67f
Minstreams <32974920+Minstreams@users.noreply.github.com>
2023-02-03 23:59:22 +0800

    对象层级

---
+++ Assets/Scripts/Framework/Hurtable.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace IceEngine
+{
+    /// <summary>
+    /// 有血条的物体
+    /// </summary>
+    public abstract class Hurtable : Selectable
+    {
+        [Group("生命")]
+        public float maxHp;
+
+        public float HP { get; set; }
+    }
+}
