08fb50a9eb02551f5c698ebcdcf8c55f9d60cf35
Minstreams <32974920+Minstreams@users.noreply.github.com>
2023-02-03 22:14:55 +0800

    GMap

---
+++ Assets/Scripts/GMap.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using IceEngine;
+
+// 游戏运行时地图
+public class GMap
+{
+    public int Width { get; private set; }
+    public int Height { get; private set; }
+
+    public GMap(int width, int height)
+    {
+        Width = width;
+        Height = height;
+
+        tex = new Texture2D(width, height);
+    }
+
+    // actual data
+    Texture2D tex;
+
+    public Color this[int x, int y]
+    {
+        get
+        {
+            if (x < 0 || x >= Width) x %= Width;
+            if (y < 0 || y >= Height) y %= Height;
+            return tex.GetPixel(x, y);
+        }
+        set
+        {
+            tex.SetPixel(x, y, value);
+            tex.Apply();
+        }
+    }
+}
---
+++ Assets/Scripts/GMapExtension.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 放一些寻路算法？
+/// </summary>
+public static class GMapExtension
+{
+
+}
