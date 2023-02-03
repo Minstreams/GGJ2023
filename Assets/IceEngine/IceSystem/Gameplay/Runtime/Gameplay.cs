9701481d7ac40778d517623253a962ce61188c16
Minstreams <32974920+Minstreams@users.noreply.github.com>
2023-02-03 22:28:53 +0800

    up

--- Assets/IceEngine/IceSystem/Gameplay/Runtime/Gameplay.cs
+++ Assets/IceEngine/IceSystem/Gameplay/Runtime/Gameplay.cs
@@ -2,6 +2,14 @@
 {
     public sealed class Gameplay : IceEngine.Framework.IceSystem<IceEngine.Internal.SettingGameplay>
     {
+        #region 玩家数据
+        // TODO: 存档问题
+        #endregion
+
+        static void Start()
+        {
+            Log("Start");
+        }
 
     }
 }
