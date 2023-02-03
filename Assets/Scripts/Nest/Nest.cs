3ed1ca9d024be7f8ad5ccf5c89004311655cdfb2
Minstreams <32974920+Minstreams@users.noreply.github.com>
2023-02-03 23:51:57 +0800

    BUilding & Nest

---
+++ Assets/Scripts/Nest/Nest.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace IceEngine
+{
+    /// <summary>
+    /// 敌人巢穴
+    /// </summary>
+    public class Nest : MonoBehaviour
+    {
+    }
+}
