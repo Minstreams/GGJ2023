44c98c9553017d53ea95861a6db7cf08d1c55f2b
Minstreams <32974920+Minstreams@users.noreply.github.com>
2023-02-05 11:48:43 +0800

    Update GameplayDrawer.cs

--- Assets/IceEngine/IceSystem/Gameplay/Editor/GameplayDrawer.cs
+++ Assets/IceEngine/IceSystem/Gameplay/Editor/GameplayDrawer.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 
 using IceEngine;
+using UnityEditor;
+using UnityEngine;
 using static IceEditor.IceGUI;
 using static IceEditor.IceGUIAuto;
 using Sys = Ice.Gameplay;
@@ -13,6 +15,38 @@ namespace IceEditor.Internal
         public override void OnToolBoxGUI()
         {
             Label("Gameplay");
+        }
+    }
+
+    public static class GameplayTools
+    {
+        [MenuItem("一键落地 #D")]
+        public static void PutOnGround()
+        {
+            foreach (var o in Selection.objects)
+            {
+                if (o is GameObject g)
+                {
+                    foreach (var mo in g.GetComponents<MapObject>())
+                    {
+                        mo.PutOnGround();
+                    }
+                }
+            }
+        }
+        [MenuItem("一键落地 _#D")]
+        public static void PutOnGroundAndRotate()
+        {
+            foreach (var o in Selection.objects)
+            {
+                if (o is GameObject g)
+                {
+                    foreach (var mo in g.GetComponents<MapObject>())
+                    {
+                        mo.PutOnGroundAndRotate();
+                    }
+                }
+            }
         }
     }
 }
