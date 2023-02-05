c0bbab25a5bc23e5b5d0d0a68b89ffd927427135
Minstreams <32974920+Minstreams@users.noreply.github.com>
2023-02-05 17:11:42 +0800

    Update Gameplay.cs

--- Assets/IceEngine/IceSystem/Gameplay/Runtime/Gameplay.cs
+++ Assets/IceEngine/IceSystem/Gameplay/Runtime/Gameplay.cs
@@ -17,10 +17,6 @@ namespace Ice
             Log("Start");
             Money = Setting.initMoney;
         }
-        static void Quitting()
-        {
-            map = null;
-        }
 
         #region 玩家数据
         static int _money;
