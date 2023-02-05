dc36ddc2072068b64cdce2b2b8a6f52bae97194d
Minstreams <32974920+Minstreams@users.noreply.github.com>
2023-02-05 17:54:00 +0800

    Update Houmen.cs

--- Assets/Scripts/Houmen.cs
+++ Assets/Scripts/Houmen.cs
@@ -6,28 +6,38 @@ namespace IceEngine
 {
     public class Houmen : MonoBehaviour
     {
+        public GameObject miwu;
         public static bool godMode = false;
         void Update()
         {
             if (Input.GetKeyDown(KeyCode.Q))
             {
+                // Q 一键解锁
                 Ice.Gameplay.UnlockKey("1");
                 Ice.Gameplay.UnlockKey("2");
                 Ice.Gameplay.UnlockKey("3");
             }
             if (Input.GetKeyDown(KeyCode.G))
             {
+                // G 无敌模式
                 godMode = !godMode;
             }
             if (Input.GetKeyDown(KeyCode.M))
             {
+                // M 加钱
                 Ice.Gameplay.Money += 10000;
             }
             if (Input.GetKeyDown(KeyCode.R))
             {
+                // R 复活
                 PanelSelectedObject.Instance.gameObject.SetActive(true);
                 PanelStatus.Instance.gameObject.SetActive(true);
                 PanelGameOver.Instance.gameObject.SetActive(false);
+            }
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                // W开关迷雾
+                miwu.SetActive(!miwu.activeSelf);
             }
         }
     }
